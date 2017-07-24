f82c259e8819da576572966471ea07cc44a2157d
Jason Carver <ut96caarrs@snkmail.com>
2017-07-23 18:56:24 -0700

    remove redundant requirement info

--- setup.py
+++ setup.py
@@ -60,5 +60,5 @@ setup(
     packages=find_packages(exclude=['tests', 'venv']),
 
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['psutil>5,<6', 'web3==3.10.0'],
+    install_requires=['psutil>5,<6', 'toolz<1', 'web3==3.10.0'],
 )
